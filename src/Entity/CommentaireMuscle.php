fc7b707e27a454423ccfb4c3ca70148edd1df716
max <m.oger@sonkoul-group.com>
2022-01-14 09:27:44 +0100

    implémentation commentaireMuscle

--- src/Entity/CommentaireMuscle.php
+++ src/Entity/CommentaireMuscle.php
@@ -6,10 +6,26 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\CommentaireMuscleRepository;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Doctrine\ORM\Mapping as ORM;
-
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 /**
- * @ApiResource()
+ * @ApiResource(
+ *       normalizationContext={"groups"={"commentaireMuscles:get"}, "skip_null_values" = false},
+ *  itemOperations={
+*       "get"={
+*           "normalization_context"={"groups"="commentaireMuscle:get"},
+*       },
+*       "delete"
+*   }
+ * )
  * @ORM\Entity(repositoryClass=CommentaireMuscleRepository::class)
+ * @ApiFilter(SearchFilter::class, properties={"commentaire"="exact", "muscle"="exact"}),
+ * @UniqueEntity(
+ *     fields={"commentaire", "muscle"},
+ *     errorPath="CommentaireMuscle",
+ *     message="This commentaireMuscle already exists."
+ * )
  */
 class CommentaireMuscle
 {
@@ -17,20 +33,21 @@ class CommentaireMuscle
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
-     * @Groups({"client:get"})
+     * @Groups({"client:get", "commentaireMuscles:get", "commentaireMuscle:get"})
      */
     private $id;
 
     /**
      * @ORM\ManyToOne(targetEntity=Commentaire::class, inversedBy="commentaireMuscles")
      * @ORM\JoinColumn(nullable=false)
+     * @Groups({"commentaireMuscles:get", "commentaireMuscle:get"})
      */
     private $commentaire;
 
     /**
      * @ORM\ManyToOne(targetEntity=Muscle::class, inversedBy="commentaireMuscles")
      * @ORM\JoinColumn(nullable=false)
-     * @Groups({"client:get"})
+     * @Groups({"client:get", "commentaireMuscles:get", "commentaireMuscle:get"})
      */
     private $muscle;
 
